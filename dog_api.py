7aa74451ddabf9ed34028a2da253a57f2de02fcb
MaskedGod <codegobbledygook@gmail.com>
2024-07-08 15:51:12 +0400

    data rewriting: solved, get one or  more photos done

--- dog_api.py
+++ dog_api.py
@@ -5,12 +5,13 @@ import requests
 def get_random_image() -> list[str]:
     url = "https://dog.ceo/api/breeds/image/random"
     r = requests.get(url)
-    data = r.json()
+    data = r.json()["message"]
+    
     # extract breed name
-    match = re.findall(r'breeds/([^/]+)/', data["message"])
+    match = re.findall(r'/breeds/([^/]+)/', data)
     match: str = match[0]
 
-    return [match.capitalize(), data["message"]]
+    return [match.capitalize(), data]
 
 
 def get_random_images(n=2) -> list[dict]:
@@ -20,9 +21,9 @@ def get_random_images(n=2) -> list[dict]:
     data = r.json()["message"]
 
     for row in data:
-        match = re.findall(r'breeds/([^/]+)/', row)
+        match = re.findall(r'/breeds/([^/]+)/', row)
         match: str = match[0]
-        result.append({"breed": match, "img": row})
+        result.append({"type": "photo", "media": row, "caption": match, })
 
     return result
 
--- private_chat_handler.py
+++ private_chat_handler.py
@@ -18,5 +18,20 @@ private_chat_router.message.filter(ChatTypeFilter(["private"]))
 
 @private_chat_router.message(CommandStart())
 async def start_chat(message: types.Message):
-    await message.answer_photo(photo=get_random_image()[1],caption=f"Hello, this is {get_random_image()[0]}")
-    await message.answer("Use /photo for one and /photo_(number) to get any amount of photos")
+    # await message.answer_photo(photo=get_random_image()[1],caption=f"Hello, this is {get_random_image()[0]}")
+    await message.answer("Use /photo for one\n/photo_(1-10) to get any amount of photos")
+
+
+@private_chat_router.message(F.text.startswith("/photo_"))
+async def get_photo(message: types.Message):
+    num = int(message.text.split("_")[1])
+    photos = get_random_images(num)
+    await message.answer_media_group([*photos])
+
+
+@private_chat_router.message(F.text == "/photo")
+async def get_photo(message: types.Message):
+    cap, photo = get_random_image()
+    await message.answer_photo(photo=photo,caption=cap)
+
+
